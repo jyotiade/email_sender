c507523ce2b1a992eab1e73b5bfd148451aa2570
jyoti ade <jyotiade287@gmail.com>
2024-09-01 21:50:06 +0530

    emailsender

---
+++ project/app/views.py
@@ -0,0 +1,12 @@
+#from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
+
+def home(request):
+    subject="project mail"
+    message="about project"
+    from_email="jyotiade287@gmail.com"
+    recipient_list=["jyotiade287@gmail.com"]
+    send_mail(subject,message,from_email,recipient_list)
+
+    return HttpResponse("done")
